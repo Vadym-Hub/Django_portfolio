5c6c7c7bd11182b810f7723b4707ab4ca4c81a0e
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2021-02-10 17:42:28 +0200

    'test_1'

--- config/settings/django.py
+++ config/settings/django.py
@@ -37,7 +37,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['pashynskyi.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
