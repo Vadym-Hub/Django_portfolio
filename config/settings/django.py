5fbd131b18fcd55424e04b828e44b625b5e86009
Vadym <webratel@gmail.com>
2021-02-15 15:10:36 +0200

    test

--- config/settings/django.py
+++ config/settings/django.py
@@ -200,17 +200,17 @@ EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 USE_AWS_S3 = USE_AWS_S3
 
 if USE_AWS_S3:
-    # aws settings
     AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
     AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
-    # s3 static settings
-    AWS_LOCATION = 'static'
+    AWS_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+    AWS_DEFAULT_ACL = None
+    AWS_S3_REGION_NAME = 'us-east-2'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+    STATIC_URL = AWS_URL + '/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    # s3 media settings
+    MEDIA_URL = AWS_URL + '/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     STATIC_URL = '/static/'
