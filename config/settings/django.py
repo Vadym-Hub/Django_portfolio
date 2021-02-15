d83d42cdae51f4e9f52404656df35067c2186a5c
Vadym <webratel@gmail.com>
2021-02-15 13:58:19 +0200

    test

--- config/settings/django.py
+++ config/settings/django.py
@@ -285,6 +285,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
+
 
 import django_heroku
 
